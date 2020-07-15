a0161742cdab1f765284232538129e6b4d2cb4ea
Lorenzo Paolo Cocchinone <lorenzo.cocchinone@gmail.com>
2020-07-15 19:28:45 +0200

    Aggiunto Progetto Con Maven

---
+++ src/main/webapp/scripts/ricerca.js
@@ -0,0 +1,49 @@
+function search() {
+    var l = document.getElementById("classificazione");
+    var livello = l.options[l.selectedIndex].value;
+
+    var r = document.getElementById("risorsa");
+    var risorsa = r.options[r.selectedIndex].value;
+
+
+    $.getJSON("ServletRicercaProduttori", {"livello": livello , "risorsa": risorsa} , function (object, status){
+
+        $(".row").remove();
+
+        $.getJSON("ServletDisplayRisorse", function (object_2){
+
+            let num = 0;
+            for(let x in object){
+                let risorse = 0;
+
+                for(i in object_2){
+
+                    if (object_2[i].idProduttore == object[x].id ){
+                        risorse += object_2[i].risorse;
+                    }
+
+                }
+
+
+                var riga = $("<tr class=\"row\">" +
+                    "<form action=\"ServletDettagliProduttore\" method=\"post\" id=\"prod_"+ num +"\"></form>" +
+                    "<input type=\"hidden\" form=\"prod_"+ num +"\" name=\"id\" value=\"" + object[x].id + "\">" +
+                    "<td>" + object[x].nome + " " + object[x].cognome+ "</td>" +
+                    "<td>" + object[x].anni + "</td>" +
+                    "<td>" + risorse + "</td>" +
+                    "<td>" + object[x].telefono + "</td>" +
+                    "<td><input type=\"submit\" form=\"prod_"+ num +"\" value =\"Visualizza Pagina Produttore\"></td>" +
+                    "</tr>");
+
+
+                $(".table").append(riga);
+                num++;
+
+
+            }
+
+        })
+
+
+    })
+}
